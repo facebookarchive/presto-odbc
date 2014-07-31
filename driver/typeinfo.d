bbdbb773f9c29e6db8c71fcb6b0b5d0155d1e325
Mark Isaacson <markisaa@fb.com>
2014-08-01 00:58:28 +0100

    Remove dead code
    I left some commented out code in typeinfo.d to make it easy to
    fix it later. That code is long deprecated so this PR removes it.
    There is now a Github issue to tend to the TODOs that are touched
    in this PR.

--- driver/typeinfo.d
+++ driver/typeinfo.d
@@ -345,6 +345,13 @@ alias SQL_SIGNED = SQL_FALSE;
 alias SQL_UNSIGNED = SQL_TRUE;
 enum SQL_HAS_NO_SIGN = null;
 
+/*
+ * TODO: Turns out that these should be named after the SQL type, not the Presto type
+ *       I believe that this one is named appropriately, should we need other type info
+ *       classes, bear that in mind.
+ * TODO: The information in this file has not been double-checked for correctness in a
+ *       while. It should be reviewed when this file is refactored.
+ */
 // http://msdn.microsoft.com/en-us/library/ms714632%28v=vs.85%29.aspx
 final class VarcharTypeInfoResultRow : OdbcResultRow {
     this(Nullability isNullable) {
@@ -384,7 +391,7 @@ final class VarcharTypeInfoResultRow : OdbcResultRow {
             case TypeInfoResultColumns.SQL_DATETIME_SUB:
                 return Variant(null);
             case TypeInfoResultColumns.NUM_PREC_RADIX:
-                return Variant(null);
+                return Variant(typeToNumPrecRadix(typeId));
             case TypeInfoResultColumns.INTERVAL_PRECISION:
                 return Variant(null);
             default:
@@ -393,60 +400,8 @@ final class VarcharTypeInfoResultRow : OdbcResultRow {
     }
 private:
     Nullability isNullable;
+    enum typeId = SQL_TYPE_ID.SQL_VARCHAR;
 }
-
-/*
- * TODO: Turns out that these should be named after the SQL type, not the Presto type
- *
-final class TypeInfoResultRow : OdbcResultRow {
-    this(Nullability isNullable) {
-        this.isNullable = isNullable;
-    }
-
-    Variant dataAt(int column) {
-        switch(column) {
-            case TypeInfoResultColumns.TYPE_NAME:
-            case TypeInfoResultColumns.LOCAL_TYPE_NAME:
-                return Variant("BOOLEAN");
-            case TypeInfoResultColumns.DATA_TYPE:
-            case TypeInfoResultColumns.SQL_DATA_TYPE:
-                return Variant(SQL_TYPE_ID.SQL_SMALLINT);
-            case TypeInfoResultColumns.COLUMN_SIZE:
-                return Variant(5);
-            case TypeInfoResultColumns.LITERAL_PREFIX:
-            case TypeInfoResultColumns.LITERAL_SUFFIX:
-                return Variant(null);
-            case TypeInfoResultColumns.CREATE_PARAMS:
-                return Variant(null);
-            case TypeInfoResultColumns.NULLABLE:
-                return Variant(isNullable);
-            case TypeInfoResultColumns.CASE_SENSITIVE:
-                return Variant(SQL_FALSE);
-            case TypeInfoResultColumns.SEARCHABLE:
-                return Variant(SQL_SEARCHABLE);
-            case TypeInfoResultColumns.UNSIGNED_ATTRIBUTE:
-                return Variant(SQL_SIGNED);
-            case TypeInfoResultColumns.FIXED_PREC_SCALE:
-                return Variant(SQL_FALSE);
-            case TypeInfoResultColumns.AUTO_UNIQUE_VALUE:
-                return Variant(SQL_FALSE); //Will need to look into this more.
-            case TypeInfoResultColumns.MINIMUM_SCALE:
-            case TypeInfoResultColumns.MAXIMUM_SCALE:
-                return Variant(null); //What is this? Docs unclear.
-            case TypeInfoResultColumns.SQL_DATETIME_SUB:
-                return Variant(null);
-            case TypeInfoResultColumns.NUM_PREC_RADIX:
-                return Variant(10);
-            case TypeInfoResultColumns.INTERVAL_PRECISION:
-                return Variant(null);
-            default:
-                assert(false);
-        }
-    }
-private:
-    Nullability isNullable;
-}
-*/
 
 enum TypeInfoResultColumns {
     TYPE_NAME = 1,
